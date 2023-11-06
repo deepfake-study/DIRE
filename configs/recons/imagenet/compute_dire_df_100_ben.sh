98dd2241b1e45e0eb8da40bd267f06db2ae9fb84
Peter Lorenz <peter.lorenz@itwm.fraunhofer.de>
2023-11-06 18:30:57 +0100

    .

--- configs/recons/imagenet/compute_dire_df_100_ben.sh
+++ configs/recons/imagenet/compute_dire_df_100_ben.sh
@@ -6,11 +6,6 @@ MODEL_PATH="/home/lorenzp/DeepFakeDetectors/DIRE/256x256_diffusion_uncond.pt"
 
 cd guided-diffusion
 
-SAMPLE_FLAGS="--batch_size 8 --num_samples 10000  --timestep_respacing ddim20 --use_ddim True"
-SAVE_FLAGS="--images_dir /home/lorenzp/DeepFakeDetectors/DIRE/data/gen/run_1/imagenet/resnet18/df/images_None/att_100 --recons_dir /home/lorenzp/DeepFakeDetectors/DIRE/data/gen/run_1/imagenet/resnet18/df/recons_None/att_100 --dire_dir /home/lorenzp/DeepFakeDetectors/DIRE/data/gen/run_1/imagenet/resnet18/df/dire_None/att_100"
-MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --dropout 0.1 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
-mpiexec -n 1 python compute_dire.py --model_path $MODEL_PATH $MODEL_FLAGS  $SAVE_FLAGS $SAMPLE_FLAGS --has_subfolder False
-
 
 SAMPLE_FLAGS="--batch_size 8 --num_samples 10000  --timestep_respacing ddim20 --use_ddim True"
 SAVE_FLAGS="--images_dir /home/lorenzp/DeepFakeDetectors/DIRE/data/gen/run_1/imagenet/resnet18/df/images_None/ben_100 --recons_dir /home/lorenzp/DeepFakeDetectors/DIRE/data/gen/run_1/imagenet/resnet18/df/recons_None/ben_100 --dire_dir /home/lorenzp/DeepFakeDetectors/DIRE/data/gen/run_1/imagenet/resnet18/df/dire_None/ben_100"
